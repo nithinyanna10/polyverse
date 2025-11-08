6eb596667809c63c0dd08c2d0597715d3e75b3a8
Nithin Yanna <nithinyanna10@users.noreply.github.com>
2025-11-07 22:23:38 -0500

    feat(lua): Add HTTP server module

---
+++ lua_scripting/http_server.lua
@@ -0,0 +1,10 @@
+-- Simple HTTP server implementation
+local http_server = {}
+
+function http_server.start(port, handler)
+    print("Starting HTTP server on port " .. port)
+    -- TODO: Implement actual HTTP server
+end
+
+return http_server
+
